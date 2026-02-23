df45f2932c8692eded8e3789f4c76e51962c3a31
123Shafaque <sa3980@srmist.edu.in>
2026-02-23 10:45:29 +0530

    UC1: Add welcome message module

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Input:");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Is it a Palindrome? :" + isPalindrome);
+    }
+}
